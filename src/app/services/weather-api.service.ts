77baaab56eac7721a2ffb3b6ea6a1d6d3b80c73a
fcola.mata@alu.uclm.es <fcola.mata@alu.uclm.es>
2018-05-06 14:15:35 +0200

    + WEATHER-API: added functions to weather service

--- src/app/services/weather-api.service.ts
+++ src/app/services/weather-api.service.ts
@@ -1,6 +1,39 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+const WEATHER_API_URL = environment.weatherApiURL;
+const WEATHER_API_KEY = environment.weatherApiKey;
+const MAPS_API_URL = environment.googleMapsApiURL;
+const MAPS_API_KEY = environment.googleMapsApiKey;
 
 @Injectable()
 export class WeatherApiService {
-  constructor() { }
+  constructor(public http: HttpClient) {
+    console.log("weather api service started");
+  }
+
+  getByCityName(city: string): Observable<any> {
+    return this.http.get(WEATHER_API_URL + '?q=' + city + '&lang=' + this.getBrowserLanguage() + '&units=metric&APPID=' + WEATHER_API_KEY).map((result) => {
+      return result;
+    });
+  }
+
+  getByLocation(lat: any, lon:any): Observable<any> {
+    return this.http.get(WEATHER_API_URL + '?lat=' + lat + '&lon=' + lon + '&lang=' + this.getBrowserLanguage() + '&units=metric&APPID=' + WEATHER_API_KEY).map((result) => {
+      return result;
+    });
+  }
+
+  getCityName(latLon): Observable<any> {
+    return this.http.get(MAPS_API_URL + '/geocode/json?latlng=' + latLon + '&sensor=false&' + MAPS_API_KEY).map((result) => {
+      return result;
+    });
+  }
+
+  getBrowserLanguage() {
+    return navigator.language.substring(0, 2);
+  }
 }
