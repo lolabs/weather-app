79236814ab67608191b59a27f62d63c617031558
fcola.mata@alu.uclm.es <fcola.mata@alu.uclm.es>
2018-05-06 20:57:34 +0200

    + ERROR-HANDLING: WIP adding service functions show and hide

---
+++ src/app/services/error-handler.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ErrorHandlerService {
+
+  constructor() { }
+
+  show(msg:string) {
+
+  }
+
+  hide(msg:string) {
+
+  }
+}
