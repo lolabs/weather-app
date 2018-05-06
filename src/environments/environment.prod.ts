ef8a6dfc1daad2dc4fa679fada9f3b3cb51732d1
fcola.mata@alu.uclm.es <fcola.mata@alu.uclm.es>
2018-05-06 22:06:29 +0200

    + ERROR-HANDLING: added URL to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiURL: 'http://api.openweathermap.org/data/2.5/weather',
+  weatherApiKey: '33ce66b81dd1b68640b1691b52d37de4',
+  googleMapsApiURL: 'https://maps.googleapis.com/maps/api',
+  googleMapsApiKey2: 'AIzaSyAFWpnhOaN39nj2IWcxtYGCYjs6CHX5wNk',
+  googleMapsApiKey: 'AIzaSyCdFjqsB7OqN5eoKUSCmejOPRf67VWbSYU',
 };
