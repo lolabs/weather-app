ae7900fb212ea930f3a397713e783a2313cb1632
fcola.mata@alu.uclm.es <fcola.mata@alu.uclm.es>
2018-05-06 14:13:36 +0200

    + WEATHER-API: set up environment config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiURL: 'http://api.openweathermap.org/data/2.5/weather',
+  weatherApiKey: '33ce66b81dd1b68640b1691b52d37de4',
+  googleMapsApiURL: 'https://maps.googleapis.com/maps/api',
+  googleMapsApiKey2: 'AIzaSyAFWpnhOaN39nj2IWcxtYGCYjs6CHX5wNk',
+  googleMapsApiKey: 'AIzaSyCdFjqsB7OqN5eoKUSCmejOPRf67VWbSYU',
 };
