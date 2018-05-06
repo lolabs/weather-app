3350110e585c5683700932709f0c002ba26aecf2
fcola.mata@alu.uclm.es <fcola.mata@alu.uclm.es>
2018-05-06 20:19:11 +0200

    + WEATHER-API: renamed file to scss in directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
 import {Ng4LoadingSpinnerComponent, Ng4LoadingSpinnerService} from 'ng4-loading-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'weather app';
